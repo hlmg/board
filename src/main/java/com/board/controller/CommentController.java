7217ec6c44d738c570b37c9058f912dcc50a08c3
hlmg <devhlmg@gmail.com>
2024-03-09 10:40:29 +0900

    refactor: API 반환 타입 변경

--- src/main/java/com/board/controller/CommentController.java
+++ src/main/java/com/board/controller/CommentController.java
@@ -1,18 +1,19 @@
 package com.board.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.board.dto.CommentRequest;
 import com.board.dto.CommentResponse;
 import com.board.service.CommentService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -20,23 +21,21 @@ import lombok.RequiredArgsConstructor;
 public class CommentController {
     private final CommentService commentService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/posts/{id}/comments")
-    public ResponseEntity<CommentResponse> createComment(@PathVariable("id") Long postId,
-            @RequestBody CommentRequest commentRequest) {
-        CommentResponse response = CommentResponse.from(commentService.create(postId, commentRequest));
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    public CommentResponse createComment(@PathVariable("id") Long postId,
+            @Valid @RequestBody CommentRequest commentRequest) {
+        return CommentResponse.from(commentService.create(postId, commentRequest));
     }
 
     @PutMapping("/comments/{id}")
-    public ResponseEntity<CommentResponse> updateComment(@PathVariable("id") Long id,
+    public CommentResponse updateComment(@PathVariable("id") Long id,
             @RequestBody CommentRequest commentRequest) {
-        CommentResponse response = CommentResponse.from(commentService.update(id, commentRequest));
-        return ResponseEntity.ok(response);
+        return CommentResponse.from(commentService.update(id, commentRequest));
     }
 
     @DeleteMapping("/comments/{id}")
-    public ResponseEntity<Void> updateComment(@PathVariable("id") Long id) {
+    public void updateComment(@PathVariable("id") Long id) {
         commentService.delete(id);
-        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/board/controller/PostController.java
+++ src/main/java/com/board/controller/PostController.java
@@ -6,13 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.board.dto.PostListResponse;
@@ -27,37 +27,33 @@ import lombok.RequiredArgsConstructor;
 public class PostController {
     private final PostService postService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/posts")
-    public ResponseEntity<PostResponse> createPost(@RequestBody PostRequest request) {
-        PostResponse response = PostResponse.from(postService.create(request));
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    public PostResponse createPost(@RequestBody PostRequest request) {
+        return PostResponse.from(postService.create(request));
     }
 
     @GetMapping("/posts")
-    public ResponseEntity<List<PostListResponse>> getAllPost(
+    public List<PostListResponse> getAllPost(
             @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        List<PostListResponse> response = postService.getAllPost(pageable)
+        return postService.getAllPost(pageable)
                 .stream()
                 .map(PostListResponse::from)
                 .toList();
-        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/posts/{id}")
-    public ResponseEntity<PostResponse> getPost(@PathVariable("id") Long id) {
-        PostResponse response = PostResponse.from(postService.getPost(id));
-        return ResponseEntity.ok(response);
+    public PostResponse getPost(@PathVariable("id") Long id) {
+        return PostResponse.from(postService.getPost(id));
     }
 
     @PutMapping("/posts/{id}")
-    public ResponseEntity<PostResponse> updatePost(@PathVariable("id") Long id, @RequestBody PostRequest request) {
-        PostResponse response = PostResponse.from(postService.update(id, request));
-        return ResponseEntity.ok(response);
+    public PostResponse updatePost(@PathVariable("id") Long id, @RequestBody PostRequest request) {
+        return PostResponse.from(postService.update(id, request));
     }
 
     @DeleteMapping("/posts/{id}")
-    public ResponseEntity<Void> deletePost(@PathVariable("id") Long id) {
+    public void deletePost(@PathVariable("id") Long id) {
         postService.delete(id);
-        return ResponseEntity.ok().build();
     }
 }
